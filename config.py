b9c8058efba8d391996fe31ce1f0efcaf71ca239
MrMozzu <muzammilahsan07@gmail.com>
2026-05-16 16:22:24 +0530

    Upgrade database to PostgreSQL

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
+import os 
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = "sqlite:///blog.db"
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "sqlite:///blog.db", "DATABASE_URL"
+        )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     
